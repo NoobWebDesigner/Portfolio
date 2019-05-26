972259a7c9ab50f942b69e2b5573c98ef6348ebc
326 fujimaki <326.fujimaki@gmail.com>
2019-05-27 03:59:37 +0900

    [Update]master.pugでやっていたレイアウト構造を_layout.pugに引き継ぎ。 サブディレクトリにあるpugをコンパイルできるように修正

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,48 +1,53 @@
 const mix = require('laravel-mix');
 const glob = require('glob');
-mix.pug = require('laravel-mix-pug-to-html');
+mix.pug = require('laravel-mix-pug-recursive');
 
 mix.setResourceRoot('');
 
 mix.js('src/js/app.js', 'public/js/app.js')
-    .autoload({
-        "popper.js": ['Popper', 'window.Popper', 'popper', 'window.popper'],
-        "jquery": ['$', 'window.jQuery']
-    })
+  .autoload({
+    "popper.js": ['Popper', 'window.Popper', 'popper', 'window.popper'],
+    "jquery": ['$', 'window.jQuery']
+  })
+
 glob.sync('src/sass/**/*.sass').map(function (file) {
-    if (file.match("/_")) return;
-    mix.sass(file, 'public/css/')
-        .options({
-            processCssUrls: false,
-            postCss: [
-                require('css-mqpacker')(),
-                require('css-declaration-sorter')({
-                    order: 'smacss'
-                })
-            ]
-        });
-    if (!mix.inProduction()) {
-        mix.webpackConfig({
-            devtool: 'inline-source-map'
+  if (file.match("/_")) return;
+  mix.sass(file, 'public/css/')
+    .options({
+      processCssUrls: false,
+      postCss: [
+        require('css-mqpacker')(),
+        require('css-declaration-sorter')({
+          order: 'smacss'
         })
-            .sourceMaps()
-    }
+      ]
+    });
+  if (!mix.inProduction()) {
+    mix.webpackConfig({
+      devtool: 'inline-source-map'
+    })
+      .sourceMaps()
+  }
 });
-glob.sync('src/pug/**/*.pug').map(function (file) {
-    if (file.match("/_")) return;
-    if (!mix.inProduction()) {
-        mix.pug(file, 'public/', {
-            pug: {
-                pretty: true,
-                debug: true
-            }
-        });
-    } else {
-        mix.pug(file, 'public/')
-    }
+glob.sync('src/**/*.pug').map(function (file) {
+  if (file.match("/_")) return;
+  if (!mix.inProduction()) {
+    mix.pug(file, 'public', {
+      excludePath: 'src/pug',
+      pug: {
+        pretty: true,
+        debug: true
+      }
+    })
+  } else {
+    mix.pug(file, 'public', {
+      excludePath: 'src/pug'
+    })
+  }
 });
+
 mix.browserSync({
-    files: './**/*',
-    server: './public/',
-    proxy: false
+  files: './**/*',
+  server: './public/',
+  proxy: false
 });
