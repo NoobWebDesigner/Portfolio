2bc73335986750fee238f436cb78ef5c9e704ce0
326 fujimaki <326.fujimaki@gmail.com>
2020-07-20 00:16:47 +0900

    [update] Laravel-mix update, PurgeCSS を導入

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,40 +1,46 @@
-const mix = require('laravel-mix');
-const glob = require('glob');
-mix.pug = require('laravel-mix-pug-recursive');
+const mix = require("laravel-mix");
+const glob = require("glob");
 
-mix.setResourceRoot('');
+require("laravel-mix-purgecss");
 
-mix.js('src/js/app.js', 'public/js/app.js')
-  .sourceMaps(false);
+mix.setResourceRoot("");
 
-glob.sync('src/sass/**/*.sass').map(function (file) {
+mix.js("src/js/app.js", "public/js/app.js").sourceMaps(false);
+
+glob.sync("src/sass/**/*.sass").map(function(file) {
   if (file.match("/_")) return;
-  mix.sass(file, 'public/css/')
-    .options({
-      processCssUrls: false,
-      postCss: [
-        require('css-mqpacker')(),
-        require('css-declaration-sorter')({
-          order: 'smacss'
-        })
-      ]
-    });
+  mix.sass(file, "public/css/").options({
+    processCssUrls: false,
+    postCss: [
+      require("css-mqpacker")(),
+      require("css-declaration-sorter")({
+        order: "smacss",
+      }),
+    ],
+  });
   if (!mix.inProduction()) {
-    mix.webpackConfig({
-      devtool: 'inline-source-map'
-    })
-      .sourceMaps()
+    mix
+      .webpackConfig({
+        devtool: "inline-source-map",
+      })
+      .sourceMaps();
   }
 });
-// glob.sync('src/**/*.pug').map(function (file) {
-//   if (file.match("/_")) return;
-//   mix.pug(file, 'public', {
-//     excludePath: 'src/pug'
-//   })
-// });
+
+mix.purgeCss({
+  // production時のみ有効化
+  enabled: mix.inProduction(),
+  extend: {
+    content: [
+      path.join(__dirname, "public/**/*.html"),
+      path.join(__dirname, "public/**/*.js"),
+    ],
+    whitelistPatterns: [/hljs/],
+  },
+});
 
 mix.browserSync({
-  files: './**/*',
-  server: './public/',
-  proxy: false
+  files: "./**/*",
+  server: "./public/",
+  proxy: false,
 });
